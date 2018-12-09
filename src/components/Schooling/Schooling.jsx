2eb227479bd79dec1ed7010006df96a8cd39c83e
David Benicek <davidbenicek@hotmail.com>
2018-12-09 14:26:09 +0100

    School order on mobile

--- src/components/Schooling/Schooling.jsx
+++ src/components/Schooling/Schooling.jsx
@@ -39,10 +39,46 @@ const SCHOOLS = [
     period: '(Jul. 2018 - Aug. 2018)'
   },
 ];
+const MOBILE_SCHOOLS = [
+  {
+    name: 'King`s College London',
+    degree: 'Msc. Advanced Software Engineering w/ Management',
+    grade: 'First Class Degree',
+    period: '(Sep. 2017 - Sep. 2018)'
+  },
+  {
+    name: 'Fudan University, Shangai',
+    degree: 'Internationl Summer Session',
+    period: '(Jul. 2018 - Aug. 2018)'
+  },
+  {
+    name: 'University of Glasgow',
+    degree: 'Bsc. Business and Management and Computing Science',
+    grade: 'First Class Degree',
+    period: '(Sep. 2013 - Jun. 2017)'
+  },
+  {
+    name: 'Chinese University of Hong Kong',
+    degree: 'Year Abroad',
+    period: '(Aug. 2014 - Jun. 2015)'
+  },
+  {
+    name: 'International School of Zug and Switzerland',
+    degree: 'International Baccalaureate and Advance Placement courses',
+    grade: 'IB: 37/45, 4/5 AP Comp. Sci.',
+    period: '(Sep. 2008 - Jun. 2013)'
+  },
+];
 
 class Schooling extends React.Component {
+  getSchools() {
+    const width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+    return (width >= 512) ? SCHOOLS : MOBILE_SCHOOLS;
+  }
   renderSchoolingOption() {
-    return SCHOOLS.map((school) => (
+    return this.getSchools().map((school) => (
       <BpkGridColumn width={6} mobileWidth={12} className={c('Schooling__box')}>
         <BpkText tagName="h3" textStyle="lg" >{school.name}</BpkText>
         <BpkText tagName="h4" textStyle="base" >{school.degree}</BpkText>
