b362e26d6db5154672b3a2bde3acef3f53e65f23
David Benicek <davidbenicek@hotmail.com>
2019-01-17 19:29:14 +0100

    Fix for Safari intersection observer

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import 'bpk-stylesheets';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+require('intersection-observer');
+
+// eslint-disable-next-line import/first
 import App from './App';
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
